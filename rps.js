aafb7ef107927c5453d3b97cd8041e590d537643
ryan <ryangviray@gmail.com>
2023-02-18 11:26:30 -0800

    getComputerChoice function

--- rps.js
+++ rps.js
@@ -1,3 +1,19 @@
-function hello() {
-    console.log("Hello World!")
+function getComputerChoice() {
+    let num1 = Math.random();
+    if (num1 <= 0.333) {
+        return "ROCK";
+    }else if (num1 > 0.333 && num1 <= 0.666) {
+        return "PAPER";
+    }else if (num1 > 0.666) {
+        return "SCISSORS";
+    }
 }
+
+
+function playRound(playerSelection, computerSelection) {
+    
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
