eb19fedc315523c5cf7ed19ad887aa1c71073534
ryan <ryangviray@gmail.com>
2023-02-28 16:27:06 -0800

    getPlayerChoice function and fixed bracket errors

--- rps.js
+++ rps.js
@@ -1,4 +1,5 @@
 function getComputerChoice() {
+// This function randomly returns rock, paper, or scissors.
     let num1 = Math.random();
     if (num1 <= 0.333) {
         return "ROCK";
@@ -9,11 +10,43 @@ function getComputerChoice() {
     }
 }
 
-
-function playRound(playerSelection, computerSelection) {
-    
+function getPlayerChoice() {
+// This function takes a valid input from the user
+    const ask = prompt(["Rock, Paper, or Scissors?"]).toUpperCase();
+    if (ask === 'ROCK'){
+        return 'ROCK'
+    } else if (ask === 'PAPER'){
+        return 'PAPER'
+    } else if (ask === 'SCISSORS'){
+        return 'SCISSORS'
+    }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "TIE";
+    } else if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
+        return "Win"
+    } else if (playerSelection === "ROCK" && computerSelection === "PAPER") {
+        return "Lose"
+    } else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
+        return "Win"
+    } else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
+        return "Lose"
+    } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
+        return "Win"
+    } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
+        return "Lose"
+    }
+}
+    
+const playerSelection = getPlayerChoice;
+const computerSelection = getComputerChoice;
 console.log(playRound(playerSelection, computerSelection));
+    
+for (let i = 0; i < 5; i++) {
+        // your code here
+}
+    
+function game() {
+}
